67ad0ed9ece94feaacd42147081c00d3db1c80b8
AyushThakali <thakaliaayush@gmail.com>
2026-05-28 20:47:56 +0545

    chat container setup done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -6,7 +6,7 @@ export const useChatStore = create((set, get) => ({
   allContacts: [],
   chats: [],
   messages: [],
-  activeTab: [],
+  activeTab: "chats",
   selectedUser: null,
   isUsersLoading: false,
   isMessageLoading: false,
@@ -41,6 +41,18 @@ export const useChatStore = create((set, get) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isUsersLoading: false });
+    }
+  },
+
+  getMessagesByUserId: async (userId) => {
+    set({ isMessageLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
+    } finally {
+      set({ isMessageLoading: false });
     }
   },
 }));
