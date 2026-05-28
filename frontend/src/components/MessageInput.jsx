bb74c0049ae694d7b6218f11be6d9a87c8847398
AyushThakali <thakaliaayush@gmail.com>
2026-05-28 20:59:53 +0545

    keyboardSound hook created

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import useKeyboardSound from "../hooks/useKeyboardSound";
+
 function MessageInput() {
+  const { playRandomKeyStrokeSound } = useKeyboardSound();
+  const [text, setText] = useState("");
+  const [imageP]
   return <div>MessageInput</div>;
 }
 
