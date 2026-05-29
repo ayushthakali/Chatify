924685fdb82982989850a3ed3a61181a9c750190
AyushThakali <thakaliaayush@gmail.com>
2026-05-29 06:18:45 +0545

    creadted functions

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,11 +1,47 @@
 import { useState } from "react";
 import useKeyboardSound from "../hooks/useKeyboardSound";
+import { useChatStore } from "../store/useChatStore";
+import toast from "react-hot-toast";
 
 function MessageInput() {
   const { playRandomKeyStrokeSound } = useKeyboardSound();
   const [text, setText] = useState("");
-  const [imageP]
-  return <div>MessageInput</div>;
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  const { sendMessage, isSoundEnabled } = useChatStore();
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (!text.trim() && !imagePreview) return;
+    if (isSoundEnabled) playRandomKeyStrokeSound();
+
+    sendMessage({ text: text.trim(), image: imagePreview });
+    setText("");
+    setImagePreview("");
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file.type.startsWith("image/*")) {
+      toast.error("Please select an image file.");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onloadend = () => setImagePreview(reader.result);
+    reader.readAsDataURL(file);
+  };
+
+  const removeImage = () => {
+    setImagePreview(null);
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  return <div className="p-4 border-t border-slate-700/50">{
+    imagePreview &&  }</div>;
 }
 
 export default MessageInput;
